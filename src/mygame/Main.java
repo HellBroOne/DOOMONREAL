aabb354c8bb96b8d262df212b23b8002edffda0d
Gerardo Rivas Delgado <gerardosmash2003@hotmail.com>
2025-06-02 22:12:10 -0600

    Ya los enemigos se mueren y dan puntos

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -1,6 +1,8 @@
 package mygame;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.audio.AudioData;
+import com.jme3.audio.AudioNode;
 import com.jme3.bounding.BoundingBox;
 import com.jme3.input.KeyInput;
 import com.jme3.input.MouseInput;
@@ -9,6 +11,7 @@ import com.jme3.math.*;
 import com.jme3.scene.*;
 import com.jme3.scene.shape.*;
 import com.jme3.material.Material;
+import com.jme3.system.AppSettings;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,11 +24,20 @@ public class Main extends SimpleApplication implements ActionListener {
     private List<Geometry> balas = new ArrayList<>();
     private List<Geometry> enemigos = new ArrayList<>();
     private Material materialBala;
+    private Material texturaCalle;
     private Material materialEnemigo;
+    private boolean salto = false;
     private int puntuacion = 0;
 
     public static void main(String[] args) {
+        AppSettings settings = new AppSettings(true);
+        settings.setTitle("DooMonreal");
+        settings.setSettingsDialogImage("Interface/dm_splash.png");
+        settings.setResolution(1280, 960);
+        settings.setFrameRate(70);
+        settings.setFullscreen(true);
         Main app = new Main();
+        app.setSettings(settings);
         app.start();
     }
 
@@ -34,6 +46,11 @@ public class Main extends SimpleApplication implements ActionListener {
         flyCam.setEnabled(false);
         cam.setLocation(new Vector3f(0, 20, 30));
         cam.lookAt(new Vector3f(0, 0, 0), Vector3f.UNIT_Y);
+        
+        AudioNode music = new AudioNode(assetManager, "Sounds/doomonreal_bgm.wav", AudioData.DataType.Stream);
+        music.setPositional(false); //PARA QUE NO SUENE OGT
+        music.setLooping(true);
+        music.play();
 
         configurarControles();
 
@@ -42,7 +59,7 @@ public class Main extends SimpleApplication implements ActionListener {
 
         materialEnemigo = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         materialEnemigo.setColor("Color", ColorRGBA.Red);
-
+        
         crearSuelo();
         crearPuente();
         crearJugador();
@@ -55,18 +72,22 @@ public class Main extends SimpleApplication implements ActionListener {
         inputManager.addMapping("Atras", new KeyTrigger(KeyInput.KEY_S));
         inputManager.addMapping("Izquierda", new KeyTrigger(KeyInput.KEY_A));
         inputManager.addMapping("Derecha", new KeyTrigger(KeyInput.KEY_D));
-        inputManager.addListener(this, "Adelante", "Atras", "Izquierda", "Derecha");
+        inputManager.addMapping("Salto", new KeyTrigger(KeyInput.KEY_SPACE));
+        inputManager.addListener(this, "Adelante", "Atras", "Izquierda", "Derecha", "Salto");
 
         inputManager.addMapping("Disparar", new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
         inputManager.addListener(this, "Disparar");
     }
 
     private void crearSuelo() {
-        Box suelo = new Box(200, 0.1f, 200);
+        Box suelo = new Box(200, 0.1f, 50);
         Geometry geomSuelo = new Geometry("Suelo", suelo);
         Material matSuelo = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        //TEXTURA DE CALLE 
+        texturaCalle = new Material( assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        texturaCalle.setTexture("ColorMap", assetManager.loadTexture("Textures/calle1.jpg"));
         matSuelo.setColor("Color", new ColorRGBA(0.2f, 0.2f, 0.3f, 1));
-        geomSuelo.setMaterial(matSuelo);
+        geomSuelo.setMaterial(texturaCalle);
         geomSuelo.setLocalTranslation(0, -0.5f, 0);
         geomSuelo.setModelBound(new BoundingBox());
         geomSuelo.updateModelBound();
@@ -122,14 +143,14 @@ public class Main extends SimpleApplication implements ActionListener {
         Sphere balaShape = new Sphere(20, 20, 0.3f);
         Geometry bala = new Geometry("Bala", balaShape);
         bala.setMaterial(materialBala);
-        Vector3f posicionBala = jugador.getLocalTranslation().add(0, 1.5f, 0);
+        Vector3f posicionBala = jugador.getLocalTranslation().add(0, 0.87f, 0);
         bala.setLocalTranslation(posicionBala);
         bala.setModelBound(new BoundingBox());
         bala.updateModelBound();
         rootNode.attachChild(bala);
         balas.add(bala);
     }
-
+    
     @Override
     public void onAction(String nombre, boolean estaPresionado, float tpf) {
         if (nombre.equals("Adelante")) adelante = estaPresionado;
@@ -171,6 +192,10 @@ public class Main extends SimpleApplication implements ActionListener {
                     System.out.println("Intersección detectada entre bala y enemigo!");
                     rootNode.detachChild(enemigo);
                     enemigos.remove(j);
+                    AudioNode kill = new AudioNode(assetManager, "Sounds/enemydefeat.wav", AudioData.DataType.Buffer);
+                    kill.setPositional(true); 
+                    kill.setLooping(false);
+                    kill.playInstance();  
                     rootNode.detachChild(bala);
                     balas.remove(i);
                     puntuacion++;
@@ -205,5 +230,6 @@ public class Main extends SimpleApplication implements ActionListener {
         rootNode.attachChild(enemigo);
         enemigos.add(enemigo);
     }
+    
 
 }
